9ce2765a1f4e1257e1cf050f28e5bb0255fec060
alexl0 <alexleon8898@gmail.com>
2021-09-06 00:44:22 +0200

    el usuario se guarda en sesino por lo que ahora aunque el usuario cierre la aplicacion no tendra que volver a iniciar sesion

--- PassengerApp/app/src/main/java/com/example/passengerapp/AuthActivity.java
+++ PassengerApp/app/src/main/java/com/example/passengerapp/AuthActivity.java
@@ -3,11 +3,14 @@ package com.example.passengerapp;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -19,6 +22,8 @@ import com.google.firebase.auth.FirebaseUser;
 public class AuthActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
+
+    LinearLayout authLayout;
 
     Button signUpButton;
     Button logInButton;
@@ -33,22 +38,35 @@ public class AuthActivity extends AppCompatActivity {
         // Initialize Firebase Auth
         mAuth = FirebaseAuth.getInstance();
 
+        authLayout = findViewById(R.id.authLayout);
+
         signUpButton = findViewById(R.id.signUpButton);
         logInButton = findViewById(R.id.logInButton);
         emailEditText = findViewById(R.id.emailEditText);
         passwordEditText = findViewById(R.id.passwordEditText);
 
         setup();
+        session();
+    }
+
+    private void session() {
+        SharedPreferences prefs = getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE);
+        String email = prefs.getString("email", null);
+        String provider = prefs.getString("provider", null);
+        if(email!=null && provider!=null){
+            // Check if user is signed in (non-null) and update UI accordingly.
+            FirebaseUser currentUser = mAuth.getCurrentUser();
+            if(currentUser != null) {
+                openMainActivity(currentUser, MainActivity.ProviderType.BASIC);
+                authLayout.setVisibility(View.INVISIBLE);
+            }
+        }
     }
 
     @Override
     public void onStart() {
         super.onStart();
-        // Check if user is signed in (non-null) and update UI accordingly.
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        if(currentUser != null){
-            openMainActivity(currentUser, MainActivity.ProviderType.BASIC);
-        }
+        authLayout.setVisibility(View.VISIBLE);
     }
 
     private void setup() {
