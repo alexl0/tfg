8cd592ed8adc33ca75328901f29081b1404c86e0
alexl0 <alexleon8898@gmail.com>
2021-09-06 01:51:32 +0200

    parte final añadir google todo funcionando correctamente

--- PassengerApp/app/src/main/java/com/example/passengerapp/AuthActivity.java
+++ PassengerApp/app/src/main/java/com/example/passengerapp/AuthActivity.java
@@ -1,27 +1,39 @@
 package com.example.passengerapp;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class AuthActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
+    private GoogleSignInClient mGoogleSignInClient;
+    private final int GOOGLE_SIGN_IN = 100;
 
     LinearLayout authLayout;
 
@@ -29,6 +41,7 @@ public class AuthActivity extends AppCompatActivity {
     Button logInButton;
     EditText emailEditText;
     EditText passwordEditText;
+    AppCompatButton googleButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +57,7 @@ public class AuthActivity extends AppCompatActivity {
         logInButton = findViewById(R.id.logInButton);
         emailEditText = findViewById(R.id.emailEditText);
         passwordEditText = findViewById(R.id.passwordEditText);
+        googleButton = findViewById(R.id.googleButton);
 
         setup();
         session();
@@ -93,8 +107,30 @@ public class AuthActivity extends AppCompatActivity {
                 }
             }
         });
+
+        googleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Configure Google Sign In
+                GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                        .requestIdToken(getString(R.string.default_web_client_id))
+                        .requestEmail()
+                        .build();
+
+                mGoogleSignInClient = GoogleSignIn.getClient(AuthActivity.this, gso);
+                //Por si tenemos mas de una cuenta de google asociada a nuestro dispositivo android,
+                //cerramos la que haya antes
+                mGoogleSignInClient.signOut();
+                startActivityForResult(mGoogleSignInClient.getSignInIntent(),GOOGLE_SIGN_IN);
+            }
+        });
     }
 
+    /**
+     * Create account with email and password
+     * @param email
+     * @param password
+     */
     private void createAccount(String email, String password) {
         mAuth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
@@ -115,6 +151,11 @@ public class AuthActivity extends AppCompatActivity {
                 });
     }
 
+    /**
+     * Sign in with email and password
+     * @param email
+     * @param password
+     */
     private void signIn(String email, String password) {
         mAuth.signInWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
@@ -136,6 +177,53 @@ public class AuthActivity extends AppCompatActivity {
     }
 
     /**
+     * Sign in with google
+     * @param requestCode
+     * @param resultCode
+     * @param data
+     */
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == GOOGLE_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                //Log.d(TAG, "firebaseAuthWithGoogle:" + account.getId());
+                firebaseAuthWithGoogle(account.getIdToken());
+            } catch (ApiException e) {
+                // Google Sign In failed, update UI appropriately
+                //Log.w(TAG, "Google sign in failed", e);
+            }
+        }
+    }
+    private void firebaseAuthWithGoogle(String idToken) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            //Log.d(TAG, "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            openMainActivity(user, MainActivity.ProviderType.GOOGLE);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            //Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            //updateUI(null);
+                            Toast.makeText(AuthActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+    }
+
+
+    /**
      * Changes the screen to the main screen of the app
      * @param user the user
      * @param provider for example, google, twitter, github, email and passwd, facebook, etc
@@ -146,4 +234,6 @@ public class AuthActivity extends AppCompatActivity {
                 .putExtra("provider", provider);
         startActivity(intent);
     }
+
+
 }
