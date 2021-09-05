64cb8401f012de1e2851f256dcb80ad6b121bfc4
alexl0 <alexleon8898@gmail.com>
2021-09-05 23:33:00 +0200

    sign in, log in, log out implemented successfuly with firebase

---
+++ PassengerApp/app/src/main/java/com/example/passengerapp/AuthActivity.java
@@ -0,0 +1,131 @@
+package com.example.passengerapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class AuthActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+
+    Button signUpButton;
+    Button logInButton;
+    EditText emailEditText;
+    EditText passwordEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auth);
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
+        signUpButton = findViewById(R.id.signUpButton);
+        logInButton = findViewById(R.id.logInButton);
+        emailEditText = findViewById(R.id.emailEditText);
+        passwordEditText = findViewById(R.id.passwordEditText);
+
+        setup();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            openMainActivity(currentUser, MainActivity.ProviderType.BASIC);
+        }
+    }
+
+    private void setup() {
+        setTitle(getString(R.string.authentication));
+
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = emailEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
+                if(!email.isEmpty() && !password.isEmpty()){
+                    createAccount(email, password);
+                }
+            }
+        });
+
+        logInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = emailEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
+                if(!email.isEmpty() && !password.isEmpty()){
+                    signIn(email, password);
+                }
+            }
+        });
+    }
+
+    private void createAccount(String email, String password) {
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            //Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            openMainActivity(user, MainActivity.ProviderType.BASIC);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            //Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(AuthActivity.this, getString(R.string.authenticationFailed),
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+    }
+
+    private void signIn(String email, String password) {
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            //Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            openMainActivity(user, MainActivity.ProviderType.BASIC);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            //Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(AuthActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+    }
+
+    /**
+     * Changes the screen to the main screen of the app
+     * @param user the user
+     * @param provider for example, google, twitter, github, email and passwd, facebook, etc
+     */
+    private void openMainActivity(FirebaseUser user, MainActivity.ProviderType provider) {
+        Intent intent = new Intent(this, MainActivity.class)
+                .putExtra("user", user)
+                .putExtra("provider", provider);
+        startActivity(intent);
+    }
+}
--- PassengerApp/app/src/main/java/com/example/passengerapp/MainActivity.java
+++ PassengerApp/app/src/main/java/com/example/passengerapp/MainActivity.java
@@ -11,22 +11,38 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
+    private FirebaseAuth mAuth;
+
+    public enum ProviderType {
+        BASIC
+    }
+
     private ConstraintLayout parent;
     private Button btnTesting;
+    private TextView textViewWelcome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
         parent = findViewById(R.id.parent);
         btnTesting = findViewById(R.id.btnTesting);
+        textViewWelcome = findViewById(R.id.textViewWelcome);
+
+        setUp();
 
         btnTesting.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,9 +53,17 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private void openNearbyDevicesActivity() {
-        Intent intent = new Intent(this, NearbyDevicesActivity.class);
-        startActivity(intent);
+    private void setUp() {
+        if(getIntent().hasExtra("user")){
+            FirebaseUser user = (FirebaseUser) getIntent().getExtras().get("user");
+            if(getIntent().hasExtra("provider")){
+                ProviderType provider = (ProviderType) getIntent().getExtras().get("provider");
+                textViewWelcome.setText(getString(R.string.welcome) + ", " + user.getEmail() + " (" + provider.name() + ")");
+            } else{
+                textViewWelcome.setText(user.getDisplayName());
+            }
+        } else
+            textViewWelcome.setText("");
     }
 
     //Mostrar Snackbar (como un cuadro de dialogo abajo tipo system.out.println o system.out)
@@ -70,7 +94,27 @@ public class MainActivity extends AppCompatActivity {
             case R.id.settings_menu:
                 Toast.makeText(this, "Settings selected", Toast.LENGTH_SHORT).show();
                 return true;
+            case R.id.logout_menu:
+                mAuth.signOut();
+                //openAuthActivity();
+                onBackPressed();
+                Toast.makeText(this, "Settings selected", Toast.LENGTH_SHORT).show();
+                return true;
+
         }
         return super.onOptionsItemSelected(item);
     }
+
+    /**
+     * Methods that change the screen of the app
+     */
+    private void openNearbyDevicesActivity() {
+        Intent intent = new Intent(this, NearbyDevicesActivity.class);
+        startActivity(intent);
+    }
+    private void openAuthActivity() {
+        Intent intent = new Intent(this, AuthActivity.class);
+        startActivity(intent);
+    }
+
 }
