38db3b85942a41b28f751f9cc71542de3b8008ca
alexl0 <alexleon8898@gmail.com>
2021-09-15 13:10:12 +0200

    añadir menu para cerrar sesion y tal, pero no se por que ahora no se guarda la sesion, no se si ya venia de antes

--- PassengerApp2/PassengerApp/app/src/main/java/com/example/passengerapp/MainActivity.kt
+++ PassengerApp2/PassengerApp/app/src/main/java/com/example/passengerapp/MainActivity.kt
@@ -1,9 +1,17 @@
 
 package com.example.passengerapp
 
+import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
 import com.example.passengerapp.bluetooth.ChatServer
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
 
@@ -26,5 +34,41 @@ class MainActivity : AppCompatActivity() {
         super.onStop()
         ChatServer.stopServer()
     }
+
+
+    //Top bar navigation
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        //return super.onCreateOptionsMenu(menu);
+        val inflater = menuInflater
+        inflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    //Top bar navigation listener
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.settings_menu -> {
+                Toast.makeText(this, "Settings selected", Toast.LENGTH_SHORT).show()
+                return true
+            }
+            R.id.logout_menu -> {
+                //Remove data saved
+                val prefs =
+                    getSharedPreferences(getString(R.string.prefs_file), MODE_PRIVATE).edit()
+                prefs.clear()
+                prefs.apply()
+                FirebaseAuth.getInstance().signOut()
+
+                //Open auth activity
+                val intent = Intent(this, AuthActivity::class.java)
+                startActivity(intent)
+
+                Toast.makeText(this, getText(R.string.logoutSuccess), Toast.LENGTH_SHORT).show()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
 
 }
