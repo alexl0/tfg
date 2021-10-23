6774a3517a6a56230140081f1ec5be5d837cf386
alexl0 <alexleon8898@gmail.com>
2021-10-23 18:14:03 +0200

    bluetooth name working properly

--- PassengerApp2/VehicleApp/app/src/main/java/com/example/passengerapp/scan/DeviceScanFragment.kt
+++ PassengerApp2/VehicleApp/app/src/main/java/com/example/passengerapp/scan/DeviceScanFragment.kt
@@ -7,7 +7,6 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.annotation.Nullable
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
@@ -20,6 +19,16 @@ import com.example.passengerapp.databinding.FragmentDeviceScanBinding
 import com.example.passengerapp.scan.DeviceScanViewState.*
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.firestore.*
+import android.bluetooth.BluetoothAdapter
+import android.os.Handler
+import android.widget.Toast
+import android.app.AlarmManager
+
+import android.app.PendingIntent
+import android.content.Context
+
+import android.content.Intent
+import android.graphics.Color
 
 
 private const val TAG = "DeviceScanFragment"
@@ -33,6 +42,7 @@ class DeviceScanFragment : Fragment() {
 
     private var name: String? = ""
     private var plate: String? = ""
+    private var bluetoothAdapter: BluetoothAdapter? = null
 
     // This property is only valid between onCreateView and onDestroyView.
     private val binding
@@ -73,6 +83,11 @@ class DeviceScanFragment : Fragment() {
         }
 
         /**
+         * Set up bluetooth adapter to change bluetooth name later
+         */
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        /**
          * Get current user
          */
         val currentUser:FirebaseUser = SingletonClass.get().hashObjects["user"] as FirebaseUser
@@ -111,8 +126,60 @@ class DeviceScanFragment : Fragment() {
                 Log.d(TAG, "Current data: ${snapData}")
 
                 //Assign name
-                if(snapData?.get("name") !=null && snapData.get("name")!="")
+                if(snapData?.get("name") !=null && snapData.get("name")!=""){
                     name=snapData.get("name").toString()
+
+                    /**
+                     * BT rename (https://stackoverflow.com/questions/8377558/change-the-android-bluetooth-device-name)
+                     */
+                    val sNewName = name
+                    val lTimeToGiveUp_ms = System.currentTimeMillis() + 10000
+                    if (bluetoothAdapter != null) {
+                        val sOldName = bluetoothAdapter!!.name
+                        if (sOldName.equals(sNewName, ignoreCase = true) == false) {
+                            val myTimerHandler = Handler()
+                            bluetoothAdapter!!.enable()
+                            myTimerHandler.postDelayed(
+                                object : Runnable {
+                                    override fun run() {
+                                        if (bluetoothAdapter!!.isEnabled) {
+                                            bluetoothAdapter!!.name = sNewName
+                                            if (sNewName.equals(
+                                                    bluetoothAdapter!!.name,
+                                                    ignoreCase = true
+                                                )
+                                            ) {
+                                                Log.i(
+                                                    "TAG_MODULE",
+                                                    "Updated BT Name to " + bluetoothAdapter!!.name
+                                                )
+                                                //bluetoothAdapter!!.disable()
+                                                Toast.makeText(requireActivity(), getText(R.string.btChangeRestart), Toast.LENGTH_SHORT).show()
+                                                binding.needToRestartAnswerTextView.setText(R.string.needToRestartAnswerYes)
+                                                binding.needToRestartAnswerTextView.setTextColor(Color.parseColor("#ff1100"));
+                                            }
+                                        }
+                                        if (sNewName.equals(
+                                                bluetoothAdapter!!.name,
+                                                ignoreCase = true
+                                            ) == false && System.currentTimeMillis() < lTimeToGiveUp_ms
+                                        ) {
+                                            myTimerHandler.postDelayed(this, 500)
+                                            if (bluetoothAdapter!!.isEnabled) Log.i(
+                                                "TAG_MODULE",
+                                                "Update BT Name: waiting on BT Enable"
+                                            ) else Log.i(
+                                                "TAG_MODULE",
+                                                "Update BT Name: waiting for Name ($sNewName) to set in"
+                                            )
+                                        }
+                                    }
+                                }, 500
+                            )
+                        }
+                    }
+
+                }
 
                 //Assign plate
                 if(snapData?.get("plate") !=null && snapData.get("plate")!="")
